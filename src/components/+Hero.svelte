e47654cb4674b021f8b385f8f947538040eb3a83
EnterOrion <orionwesson@gmail.com>
2024-05-04 14:17:14 -0700

    Add hero

---
+++ src/components/+Hero.svelte
@@ -0,0 +1,10 @@
+<script>
+	import heroImg from '$lib/assets/nightSky.jpg';
+	import '../styles/main.scss';
+</script>
+
+<div class="hero">
+	<div class="frame">
+		<img class="hero-img" src={heroImg} alt="A full moon above snowy mountains" />
+	</div>
+</div>
