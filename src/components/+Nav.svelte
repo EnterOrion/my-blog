c3dad5494430e0144545014178796c4cd16b9d82
EnterOrion <orionwesson@gmail.com>
2024-05-04 14:16:51 -0700

    Add nav

---
+++ src/components/+Nav.svelte
@@ -0,0 +1,8 @@
+<nav>
+	<div><a href="/">On emptiness and form</a></div>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/blog">Blog</a></li>
+		<li><a href="/about">About</a></li>
+	</ul>
+</nav>
