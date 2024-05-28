eb1175eb0cfc0b4f22baf670aad9ce40950e71af
EnterOrion <orionwesson@gmail.com>
2024-05-27 19:58:50 -0700

    Fix deployment issues

--- src/components/+Nav.svelte
+++ src/components/+Nav.svelte
@@ -1,14 +1,17 @@
 <script>
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 </script>
 
 <nav>
-	<div><a href="/">⟁ On emptiness and form</a></div>
+	<div><a href={`${base}/`}>⟁ On emptiness and form</a></div>
 	<ul>
 		<li>
-			<a href="/" class:active={$page.url.pathname == '/'}><span class="special">Home</span></a>
+			<a href={`${base}/`} class:active={$page.url.pathname == '/'}
+				><span class="special">Home</span></a
+			>
 		</li>
-		<li><a href="/blog" class:active={$page.url.pathname == '/blog'}>Blog</a></li>
-		<li><a href="/about" class:active={$page.url.pathname == '/about'}>About</a></li>
+		<li><a href={`${base}/blog`} class:active={$page.url.pathname == '/blog'}>Blog</a></li>
+		<li><a href={`${base}/about`} class:active={$page.url.pathname == '/about'}>About</a></li>
 	</ul>
 </nav>
