f4a3270819a619a3dcd512b7eec07ac29b139ef4
EnterOrion <orionwesson@gmail.com>
2024-06-08 09:59:47 -0700

    Improve styling of post page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,19 +1,17 @@
 <script>
 	export let data;
 	import '../../../styles/main.scss';
-	import { base } from '$app/paths';
+	import Nav from '../../../components/+Nav.svelte';
 </script>
 
-<div>
-	<button class="back-button"><a href={`${base}/blog`}>←</a></button>
-	<article>
-		<header>
-			<h1 class="post-header">{data.post.title}</h1>
-			<div>Published on {data.post.date}</div>
-		</header>
-		<!-- render the post -->
-		<div class="post-body">
-			<svelte:component this={data.component} />
-		</div>
-	</article>
-</div>
+<Nav />
+<article>
+	<header>
+		<h1 class="post-header">{data.post.title}</h1>
+		<div>Published on {data.post.date}</div>
+	</header>
+	<!-- render the post -->
+	<div class="post-body">
+		<svelte:component this={data.component} />
+	</div>
+</article>
