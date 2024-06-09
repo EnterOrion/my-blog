0abed8bc2fdf02e232f19a76ba40c1d98baaca59
EnterOrion <orionwesson@gmail.com>
2024-06-08 18:21:00 -0700

    Basic mobile menu

--- src/components/+Nav.svelte
+++ src/components/+Nav.svelte
@@ -2,6 +2,8 @@
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import vajraIcon from '$lib/assets/vajra.png';
+
+	let mobileActive = false;
 </script>
 
 <nav>
@@ -21,4 +23,34 @@
 			<a href={`${base}/about`} class:active={$page.url.pathname == `${base}/about`}>About</a>
 		</li>
 	</ul>
+
+	<div class="hamburger-wrapper">
+		<div
+			class="hamburger"
+			tabindex="1"
+			role="button"
+			class:mobile-active={mobileActive}
+			on:click={() => (mobileActive = !mobileActive)}
+		>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
 </nav>
+
+{#if mobileActive}
+	<div class="mobile-menu">
+		<ul>
+			<li>
+				<a href={`${base}/`} class:active={$page.url.pathname == `${base}/`}
+					><span class="special">Home</span></a
+				>
+			</li>
+			<li><a href={`${base}/blog`} class:active={$page.url.pathname == `${base}/blog`}>Blog</a></li>
+			<li>
+				<a href={`${base}/about`} class:active={$page.url.pathname == `${base}/about`}>About</a>
+			</li>
+		</ul>
+	</div>
+{/if}
