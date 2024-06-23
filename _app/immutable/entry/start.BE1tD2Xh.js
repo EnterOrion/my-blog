0f6195884970306b59ff73401e96927547d82fc5
EnterOrion <orionwesson@gmail.com>
2024-06-23 12:55:30 -0700

    Updates

---
+++ _app/immutable/entry/start.BE1tD2Xh.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DXHKeF_T.js";export{t as start};
---
+++ _app/immutable/nodes/3.BZegzW3Y.js
@@ -0,0 +1,9 @@
+import{s as c,n as h}from"../chunks/scheduler.CtbWrGNo.js";import{S as p,i as u,n as d,s as f,e as v,o as w,f as y,c as g,k as _,p as b,q as I,g as r,l as $,m as k,d as l,r as x}from"../chunks/index.DxEFSMOr.js";import{N as M}from"../chunks/_Nav.C-XwLFdb.js";const j=""+new URL("../assets/me.1GebQA0v.jpg",import.meta.url).href;function A(m){let e,n,a,o=`<div class="about-content"><h1>About</h1> <div class="photo-text"><div class="frame"><img src="${j}" alt="Orion Wesson"/></div> <p>My name is Orion and I&#39;ve always been curious about the nature of reality and mind. Before
+				studying and practicing Buddhism, I had an interest in Western Occultism, ritual, and
+				philosophy. I&#39;ve always be drawn to the unknown, the esoteric, to what lies underneath the
+				veil of the mundane. I then came across Buddhism and I fell in love with its practical
+				approach to spirituality. It inspired me to work towards liberation within this very life.
+				When I discovered Vajrayana, I was happy to have a practice that combined both the
+				compassionate goal of eliminating suffering with the power and art of ritual. Similarly, I
+				believe that literature can similarly be a transformative way of shaping the mind and
+				opening it up to a broader horizon previously unknown.</p></div></div>`,i;return e=new M({}),{c(){d(e.$$.fragment),n=f(),a=v("main"),a.innerHTML=o,this.h()},l(t){w(e.$$.fragment,t),n=y(t),a=g(t,"MAIN",{class:!0,"data-svelte-h":!0}),_(a)!=="svelte-7knj31"&&(a.innerHTML=o),this.h()},h(){b(a,"class","about")},m(t,s){I(e,t,s),r(t,n,s),r(t,a,s),i=!0},p:h,i(t){i||($(e.$$.fragment,t),i=!0)},o(t){k(e.$$.fragment,t),i=!1},d(t){t&&(l(n),l(a)),x(e,t)}}}class O extends p{constructor(e){super(),u(this,e,null,A,c,{})}}export{O as component};
