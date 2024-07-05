83fa7685af29e8f78f9a43d803470a2960df8dbd
EnterOrion <orionwesson@gmail.com>
2024-07-05 10:39:21 -0700

    Add new post

--- src/lib/data/posts.js
+++ src/lib/data/posts.js
@@ -4,5 +4,11 @@ export const posts = [
 		title: 'In the Presence of the Buddha - Nembutsu',
 		date: '6/23/24',
 		summary: 'An overview of the benefits of nembutsu, no matter what school you follow.'
+	},
+	{
+		slug: 'amida-grace-bodhicitta',
+		title: 'Amida, Grace, and Bodhicitta',
+		date: '7/5/24',
+		summary: 'What role does grace play in Buddhism?'
 	}
 ];
