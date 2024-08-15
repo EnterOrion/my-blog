767912a1c032cdcc3804415e453d92f501eed661
EnterOrion <orionwesson@gmail.com>
2024-08-14 20:14:26 -0700

    Remove old posts

--- src/lib/data/posts.js
+++ src/lib/data/posts.js
@@ -1,14 +1,1 @@
-export const posts = [
-	{
-		slug: 'in-the-presence-of-the-buddha',
-		title: 'In the Presence of the Buddha - Nembutsu',
-		date: '6/23/24',
-		summary: 'An overview of the benefits of nembutsu, no matter what school you follow.'
-	},
-	{
-		slug: 'amida-grace-bodhicitta',
-		title: 'Amida, Grace, and Bodhicitta',
-		date: '7/5/24',
-		summary: 'What role does grace play in Buddhism?'
-	}
-];
+export const posts = [];
